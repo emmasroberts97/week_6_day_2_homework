915e1474566295f10dc379cb42dbf14cdee719e9
Emma Roberts <emma.roberts197@gmail.com>
2020-02-11 19:58:54 +0000

    Finished MVP homework

---
+++ models/park.js
@@ -0,0 +1,53 @@
+const Park = function(name, ticketPrice, dinosaurs){
+  this.name = name;
+  this.ticketPrice = ticketPrice;
+  this.dinosaurs = dinosaurs;
+}
+
+// Add a dinosaur to its collection of dinosaurs
+Park.prototype.addDino = function(dinosaur){
+  this.dinosaurs.push(dinosaur);
+};
+// Remove a dinosaur from its collection of dinosaurs
+Park.prototype.removeDino = function(dinosaur){
+  const dinosaurIndex = this.dinosaurs.indexOf(dinosaur)
+  this.dinosaurs.splice(dinosaurIndex, 1);
+};
+// Find the dinosaur that attracts the most visitors
+Park.prototype.popularDino = function(){
+  let max = Math.max(...this.dinosaurs.map(dinosaur => dinosaur.guestsAttractedPerDay));
+  for (const dinosaur of this.dinosaurs){
+    if (dinosaur.guestsAttractedPerDay === max){
+      return dinosaur;
+    };
+  };
+};
+// Find all dinosaurs of a particular species
+Park.prototype.dinoSpecies = function(species){
+  let dinoSpecies = [];
+  for (const dinosaur of this.dinosaurs){
+    if (dinosaur.species === species){
+      dinoSpecies.push(dinosaur);
+    };
+  };
+  return dinoSpecies;
+};
+// Calculate the total number of visitors per day
+Park.prototype.dayVisitors = function(){
+  let total = 0;
+  for (const dinosaur of this.dinosaurs){
+    total += dinosaur.guestsAttractedPerDay;
+  };
+  return total;
+};
+// Calculate the total number of visitors per year
+Park.prototype.yearVisitors = function(){
+  return this.dayVisitors() * 365;
+};
+// Calculate the total revenue from ticket sales for one year
+Park.prototype.yearRevenue = function(){
+  return this.yearVisitors() * this.ticketPrice;
+};
+
+
+module.exports = Park;
---
+++ specs/park_spec.js
@@ -0,0 +1,80 @@
+const assert = require('assert');
+const Park = require('../models/park.js');
+const Dinosaur = require('../models/dinosaur.js');
+
+describe('Park', function() {
+
+  beforeEach(function() {
+    dinosaur1 = new Dinosaur('t-rex', 'carnivore', 20);
+    dinosaur2 = new Dinosaur('pterodactyl', 'carnivore', 15);
+    dinosaur3 = new Dinosaur('diplodocus', 'herbivore', 30);
+    dinosaur4 = new Dinosaur('oviraptor', 'omnivore', 10);
+    dinosaur5 = new Dinosaur('stegosaurus', 'herbivore', 25);
+    park = new Park('Dino-mite Park', 10, [dinosaur1, dinosaur2, dinosaur3]);
+  })
+
+  it('should have a name', function() {
+    const actual = park.name;
+    const expected = 'Dino-mite Park';
+    assert.strictEqual(actual, expected);
+  });
+
+  it('should have a ticket price', function() {
+    const actual = park.ticketPrice;
+    const expected = 10;
+    assert.strictEqual(actual, expected);
+  });
+
+  it('should have a collection of dinosaurs', function() {
+    const actual = park.dinosaurs.length;
+    const expected = 3;
+    assert.strictEqual(actual, expected);
+  });
+
+  it('should be able to add a dinosaur to its collection', function() {
+    park.addDino(dinosaur4);
+    const actual = park.dinosaurs.length;
+    const expected = 4;
+    assert.strictEqual(actual, expected);
+  });
+
+  it('should be able to remove a dinosaur from its collection', function() {
+    park.addDino(dinosaur4);
+    park.addDino(dinosaur5);
+    park.removeDino(dinosaur4);
+    const actual = park.dinosaurs.length;
+    const expected = 4;
+    assert.strictEqual(actual, expected);
+  });
+
+  it('should be able to find the dinosaur that attracts the most visitors', function(){
+    const actual = park.popularDino();
+    const expected = dinosaur3;
+    assert.deepEqual(actual, expected);
+  });
+
+  it('should be able to find all dinosaurs of a particular species', function(){
+    const actual = park.dinoSpecies('t-rex');
+    const expected = [dinosaur1]
+    assert.deepEqual(actual, expected);
+  });
+
+  it('should be able to calculate the total number of visitors per day', function(){
+    const actual = park.dayVisitors();
+    const expected = 65;
+    assert.strictEqual(actual, expected);
+  });
+
+  it('should be able to calculate the total number of visitors per year', function(){
+    const actual = park.yearVisitors();
+    const expected = 23725;
+    assert.strictEqual(actual, expected);
+  });
+
+  it('should be able to calculate total revenue for one year', function(){
+    const actual = park.yearRevenue();
+    const expected = 237250;
+    assert.strictEqual(actual, expected);
+  });
+
+});
